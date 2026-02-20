edbbe60552d0877b1a29095ca4ca475ad52b3b67
PABLO JIMÉNEZ FERNÁNDEZ <pjf752@inlumine.ual.es>
2026-02-20 09:14:40 +0100

    Add Solution for EcuacionSegundoGrado.java for Session02

---
+++ Java-Learning/src/main/java-introduction/com/learning/session02/EcuacionSegundoGrado.java
@@ -0,0 +1,33 @@
+package com.learning.session02;
+
+public class EcuacionSegundoGrado {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = -3;
+		int c = 2;
+		
+		System.out.println("SOLUCION DE UNA ECUACION DE SEGUNDO GRADO"
+				+ "\rValores de los coeficientes"
+				+ "\ra = "+a+", b = "+b+", c = "+c);
+		
+		int radicando = b * b - 4 * a * c;
+		
+		if (a == 0) {
+			System.out.println("No es una ecuacion de segundo grado");
+		} else if (radicando < 0) {
+			System.out.println("No tiene solucion real");
+		} else if (radicando == 0) {
+			System.out.println("Una unica raiz de valor doble");
+			System.out.println("x = "+(-b/(2*a)));
+		} else {
+			System.out.println("Dos raices de valores");
+			System.out.println("x1 = " +(-b+Math.sqrt(radicando)) / (2*a));
+			System.out.println("x2 = " +(-b-Math.sqrt(radicando)) / (2*a));
+
+		}
+		
+	}
+
+}
